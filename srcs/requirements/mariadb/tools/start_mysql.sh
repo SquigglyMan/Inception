95a482ae779562db358fb2a5a96ac2eb87a019eb
SquigglyMan <L_D_S_L@hotmail.com>
2024-12-12 20:44:07 +0100

    master : mariadb : trying a shell script to change mysql conf file

--- srcs/requirements/mariadb/tools/start_mysql.sh
+++ srcs/requirements/mariadb/tools/start_mysql.sh
@@ -1,7 +1,9 @@
-service mysql start
+sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# service mysql start
+
+# mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+# mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+# mysql -u root -e "FLUSH PRIVILEGES;"
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
