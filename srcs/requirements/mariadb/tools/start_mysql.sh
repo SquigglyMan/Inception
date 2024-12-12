aa09bcb6026753364b4f2561286222e4c0a6bbb4
SquigglyMan <L_D_S_L@hotmail.com>
2024-12-12 23:22:08 +0100

    master : mariadb : test

--- srcs/requirements/mariadb/tools/start_mysql.sh
+++ srcs/requirements/mariadb/tools/start_mysql.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+/bin/yes
+
 # service mysql start
 
 # mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
