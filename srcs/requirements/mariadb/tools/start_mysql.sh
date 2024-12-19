b5e2631e186446bb656d313480f9a04403818a94
SquigglyMan <L_D_S_L@hotmail.com>
2024-12-19 08:46:54 +0100

    master : mariadb : running mariadb as a daemon in container

--- srcs/requirements/mariadb/tools/start_mysql.sh
+++ srcs/requirements/mariadb/tools/start_mysql.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+!/bin/bash
+
 # Either you change the configuration file to use localhost/0.0.0.0
 # sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# service mysql start
+service mariadb start
 
-# mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
-# mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-# mysql -u root -e "FLUSH PRIVILEGES;"
-# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+service mariadb stop
 
 #Or you launch the program using --bind-address=0.0.0.0 flag
+mysqld_safe --bind-address=0.0.0.0
