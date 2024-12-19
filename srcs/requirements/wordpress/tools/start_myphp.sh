c95a6bbf95b7ee674d3e61e011e392cf5eb99c18
SquigglyMan <L_D_S_L@hotmail.com>
2024-12-19 14:39:39 +0100

    master : wordpress : type when giving path to wordpress installation

--- srcs/requirements/wordpress/tools/start_myphp.sh
+++ srcs/requirements/wordpress/tools/start_myphp.sh
@@ -8,7 +8,7 @@ sed -i 's/listen\s*=\s*\/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/
 # sed -i 's/;pm.status_path = \/status/pm.status_path = \/php_fpm_status/' /etc/php/8.2/fpm/pool.d/www.conf
 
 wp core download --allow-root --path=/var/www/html/wordpress
-wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --skip-check
+wp config create --allow-root --path=/var/www/html/wordpress --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD}
 wp core install --allow-root --path=/var/www/html/wordpress --url"=https://$DOMAIN_NAME" --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 wp theme install astra --allow-root --activate 
 
