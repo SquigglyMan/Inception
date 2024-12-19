36d03f77b75faa44591886af173d7c79ca8140c7
SquigglyMan <L_D_S_L@hotmail.com>
2024-12-19 11:37:54 +0100

    master : trying to run all three containers

--- srcs/requirements/wordpress/tools/start_myphp.sh
+++ srcs/requirements/wordpress/tools/start_myphp.sh
@@ -1,1 +1,14 @@
-echo "start_myphp.sh"
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+sed -i 's/listen\s*=\s*\/run\/php\/php8.2-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/8.2/fpm/pool.d/www.conf
+# sed -i 's/;pm.status_path = \/status/pm.status_path = \/php_fpm_status/' /etc/php/8.2/fpm/pool.d/www.conf
+
+wp core download --allow-root --path=/var/www/tmlh/wordpress --quiet
+wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD}
+wp core install --allow-root --path=$path --url"=https://$DOMAIN_NAME" --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+
+php-fpm8.2
