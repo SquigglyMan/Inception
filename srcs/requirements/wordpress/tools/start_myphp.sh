a17e6ee75d82836bf7fa4084d3a9c5bb1e85e943
SquigglyMan <L_D_S_L@hotmail.com>
2024-12-21 23:26:40 +0100

    master : wordpress : container working

--- srcs/requirements/wordpress/tools/start_myphp.sh
+++ srcs/requirements/wordpress/tools/start_myphp.sh
@@ -4,14 +4,15 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-# sed -i 's/listen\s*=\s*\/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/listen\s*=\s*\/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
 # sed -i 's/;pm.status_path = \/status/pm.status_path = \/php_fpm_status/' /etc/php/8.2/fpm/pool.d/www.conf
 
 wp core download --allow-root --path=/var/www/html/wordpress
-wp config create --allow-root --path=/var/www/html/wordpress --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --skip-check
-wp core install --allow-root --path=/var/www/html/wordpress --url"=https://$DOMAIN_NAME" --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-wp theme install astra --allow-root --activate 
+cd /var/www/html/wordpress
+wp config create --allow-root --path=/var/www/html/wordpress --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb
+wp core install --allow-root --path=/var/www/html/wordpress --url"=https://$DOMAIN_NAME" --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+wp theme install astra --allow-root --path=/var/www/html/wordpress --activate
 
-wp server --port=9000
+# wp server --port=9000
 
-#php-fpm7.4 -F
+php-fpm7.4 -F -y /etc/php/7.4/fpm/php-fpm.conf
