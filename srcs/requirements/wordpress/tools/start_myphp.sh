f092efec5098a7ec9075aeb78dc2ab33b80aa587
SquigglyMan <L_D_S_L@hotmail.com>
2024-12-22 21:52:45 +0100

    master : website working but not with url

--- srcs/requirements/wordpress/tools/start_myphp.sh
+++ srcs/requirements/wordpress/tools/start_myphp.sh
@@ -5,14 +5,13 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 sed -i 's/listen\s*=\s*\/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
-# sed -i 's/;pm.status_path = \/status/pm.status_path = \/php_fpm_status/' /etc/php/8.2/fpm/pool.d/www.conf
+sed -i 's/;pm.status_path = \/status/pm.status_path = \/php_fpm_status/' /etc/php/7.4/fpm/pool.d/www.conf
 
-wp core download --allow-root --path=/var/www/html/wordpress
 cd /var/www/html/wordpress
-wp config create --allow-root --path=/var/www/html/wordpress --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb
-wp core install --allow-root --path=/var/www/html/wordpress --url"=https://$DOMAIN_NAME" --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-wp theme install astra --allow-root --path=/var/www/html/wordpress --activate
-
-# wp server --port=9000
+wp core download --allow-root --path=/var/www/html/wordpress
+wp config create --allow-root --path=/var/www/html/wordpress --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
+wp core install --allow-root --path=/var/www/html/wordpress --url=https://llarue.42.fr --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+wp user create --allow-root --path=/var/www/html/wordpress $WP_USER $WP_EMAIL --user_pass=$WP_PASSWD --role=$WP_USER_ROLE
+wp theme install saaslauncher --allow-root --path=/var/www/html/wordpress --activate
 
 php-fpm7.4 -F -y /etc/php/7.4/fpm/php-fpm.conf
